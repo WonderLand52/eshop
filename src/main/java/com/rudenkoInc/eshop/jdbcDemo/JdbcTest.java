ec99857dc0fb6b3d4b6d342661645ceeec2b1579
WonderLand52 <illiaRudenko52@gmail.com>
2015-01-23 09:45:38 +0200

    Update JdbcTest.java

--- src/main/java/com/rudenkoInc/eshop/jdbcDemo/JdbcTest.java
+++ src/main/java/com/rudenkoInc/eshop/jdbcDemo/JdbcTest.java
@@ -9,7 +9,7 @@ public class JdbcTest {
 
     public static void main(String[] args) {
         try {
-            Driver driver = DriverManager.getDriver("jdbc:mysql://127.0.0.1:3306/eshop?user=admin&password=pdtcthcndjcj52");
+            Driver driver = DriverManager.getDriver("jdbc:mysql://127.0.0.1:3306/eshop?user=admin&password=******");
             System.out.println("Driver: " + driver);
         }catch (SQLException e){
             System.out.println("Exception: " + e);
